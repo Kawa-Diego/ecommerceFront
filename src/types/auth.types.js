2eea393455665db4fb34a2bbafeb06b5a922c6bb
Diego-mah-gah <kawadiego.soares@gmail.com>
2026-06-29 01:38:02 -0300

    feat: tipagem dos valores

---
+++ src/types/auth.types.js
@@ -0,0 +1,26 @@
+/**
+ * @typedef {Object} LoginRequest
+ * @property {string} email
+ * @property {string} password
+ */
+
+/**
+ * @typedef {Object} RegisterRequest
+ * @property {string} name
+ * @property {string} email
+ * @property {string} password
+ * @property {string} cpf
+ */
+
+/**
+ * @typedef {Object} AuthResponse
+ * @property {string} name
+ * @property {string} token
+ */
+
+/**
+ * @typedef {Object} RegisterResponse
+ * @property {string} message
+ */
+
+export {};
